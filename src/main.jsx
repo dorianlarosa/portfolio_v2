0dc035d2a4907094bdd14db39b5cab6fc66d56cd
Dorian LA ROSA <larosadorian@hotmail.fr>
2024-02-02 20:06:01 +0100

    options lenis

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,14 @@ import { ReactLenis } from '@studio-freight/react-lenis'
 
 import { BrowserRouter } from 'react-router-dom'
 
+const optionsLenis = {
+  duration: 2, // Durée du défilement en secondes
+  wheelMultiplier : .7
+};
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReactLenis root>
+  <ReactLenis options={optionsLenis} root>
       <React.StrictMode>
         <BrowserRouter>
           <App />
