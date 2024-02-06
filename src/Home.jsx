d1c7c315fd7f2f09218022ca852b2961ad161041
Dorian LA ROSA <larosadorian@hotmail.fr>
2024-02-06 18:12:32 +0100

    add blob section hero

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.scss";
 import { ParallaxBanner, Parallax } from 'react-scroll-parallax';
 
 // Assurez-vous que les composants suivants sont correctement importés.
-import { Section, BadgeScroll, Button, ListTag, WaveEffectImage } from "./components";
+import { Section, BadgeScroll, Button, ListTag, WaveEffectImage, Blob } from "./components";
 
 import imageConstruction1 from './assets/images/construction-1.jpg';
 import imageConstruction2 from './assets/images/construction-2.jpg';
@@ -73,6 +73,7 @@ class Home extends Component {
     return (
       <>
         <section id="section-hero">
+          <Blob/>
           <h1>
             <span className="name">
               Dorian LA ROSA
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,3 +6,4 @@ export { default as BadgeScroll } from "./BadgeScroll/BadgeScroll";
 export { default as Button } from "./Button/Button";
 export { default as ListTag } from "./ListTags/ListTags";
 export { default as WaveEffectImage } from "./WaveEffectImage/WaveEffectImage";
+export { default as Blob } from "./Blob/Blob";
